62c9b1d1e3033b41d0feca2cf6ce4d4b48477e88
barto14753 <wlodarski_bartosz@o2.pl>
2023-09-10 19:55:25 +0200

    Fix block creation

--- blockchain-demo/src/utils/BlockUtils.jsx
+++ blockchain-demo/src/utils/BlockUtils.jsx
@@ -1,17 +1,18 @@
 import sha256 from "crypto-js/sha256";
 import { hash } from "./Hash";
 
-const MINE_DIFFICULTY = 1;
-const SEARCH_DEPTH = 100000;
+const MINE_DIFFICULTY = 2;
+const SEARCH_DEPTH = 10000;
 
 
 export const mine = (block) => {
-	const nonce = [...Array(SEARCH_DEPTH).keys()]
-		.map(nonce => sha256(nonce + block.id + block.created + block.prev))
-		.find(h => {
-            console.log(h)
-            return h.slice(MINE_DIFFICULTY) === MINE_DIFFICULTY * "0"
-        });
+    let nonce;
+    for (nonce = 0; nonce<SEARCH_DEPTH; nonce++) {
+        const hash = sha256(nonce + block.id + block.created + block.prev).toString()
+        if (hash.slice(-MINE_DIFFICULTY) === "0".repeat(MINE_DIFFICULTY)) {
+            break;
+        }
+    }
     block.nonce = nonce
     block.hash = hash(block)
     return block
