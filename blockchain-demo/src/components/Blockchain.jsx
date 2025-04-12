9377cbfc986b369941ca924d33958b54f7b67d84
bartek <wlodarski_bartosz@o2.pl>
2025-04-12 13:16:40 +0200

    Add state saving and clearing blockchain

--- blockchain-demo/src/components/Blockchain.jsx
+++ blockchain-demo/src/components/Blockchain.jsx
@@ -1,18 +1,32 @@
 import { Box, Fab } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Block from "./Block";
 import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { createRootBlock, newBlock } from "../utils/BlockUtils";
 
+const STORAGE_KEY = "blockchain_state";
 const initialBlocks = [createRootBlock()];
+
 export default function Blockchain() {
-	const [blocks, setBlocks] = useState(initialBlocks);
+	const [blocks, setBlocks] = useState(() => {
+		const saved = localStorage.getItem(STORAGE_KEY);
+		return saved ? JSON.parse(saved) : initialBlocks;
+	});
+
+	useEffect(() => {
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(blocks));
+	}, [blocks]);
 
 	const addBlock = () => {
 		const lastBlock = blocks[blocks.length - 1];
 		const block = newBlock(lastBlock, "Some data");
 		setBlocks([...blocks, block]);
 		scrollDown();
+	};
+
+	const clearBlocks = () => {
+		setBlocks(initialBlocks);
 	};
 
 	const scrollDown = () => {
@@ -37,14 +51,23 @@ export default function Blockchain() {
 						setBlocks={setBlocks}
 					/>
 				))}
-				<Fab
-					color="primary"
-					onClick={addBlock}
-					aria-label="add"
-					style={{ position: "fixed", right: "50px", bottom: "50px" }}
+				<Box
+					sx={{
+						position: "fixed",
+						right: "50px",
+						bottom: "50px",
+						display: "flex",
+						flexDirection: "column",
+						gap: 2,
+					}}
 				>
-					<AddIcon />
-				</Fab>
+					<Fab color="error" onClick={clearBlocks} aria-label="clear">
+						<DeleteIcon />
+					</Fab>
+					<Fab color="primary" onClick={addBlock} aria-label="add">
+						<AddIcon />
+					</Fab>
+				</Box>
 			</Box>
 		</>
 	);
